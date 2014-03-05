de08017badd77507a17acd7e9674feb90efe2145
Tatu Solla <tatu.solla@live.com>
2014-03-05 23:56:07 +0200

    More files

---
+++ CreateXML.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+namespace ParseSpreadsheetXML
+{
+    class CreateXML : Parser
+    {
+        private string fileName = @"D:\root.xml";
+        private XDocument doc;
+
+        public CreateXML()
+        {
+            doc = new XDocument();
+            XElement tree = new XElement("Workouts");
+            List<XElement> elements = new List<XElement>();
+
+            int offset = nodes.Count;
+
+            for (int i = 0; i < nodes.Count; ++i)
+            {
+                tree.Add(
+                    new XElement("Workout",
+                        new XElement("Pvm", getColValue(i + offset * 4)),
+                        new XElement("Movement",
+                            new XElement("Name", nodes[i].Value),
+                                new XElement("Sarjat", getColValue(i + offset)),
+                                new XElement("Toistot", getColValue(i + offset * 2)),
+                                new XElement("Painot", getColValue(i + offset * 3))))
+                    );
+            }
+
+            doc.Add(tree);
+
+            Save(doc);
+
+            Console.WriteLine(doc + "\nXML TREE CREATED");
+        }
+
+        private void Save(XDocument tree)
+        {
+            doc.Save(this.fileName, SaveOptions.None);
+        }
+    }
+}
---
+++ Parser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace ParseSpreadsheetXML
+{
+    class Parser
+    {
+        private XDocument root;
+        private IEnumerable<XElement> elements;
+        protected List<XElement> nodes;
+        protected List<string> columns;
+
+
+        private string rootAddress = @"D:\Painot.xml";
+
+        public Parser()
+        {
+            root = XDocument.Load(rootAddress);
+
+            nodes = new List<XElement>();
+            columns = new List<string>();
+
+            getNodes();
+
+            for (int i = 0; i < 6; ++i)
+            {
+                getCol(i);
+            }
+        }
+
+        private void getNodes()
+        {
+            elements =
+                    from el in root.Descendants("ooo_row")
+                    select el;
+
+            foreach (XElement el in elements)
+                nodes.Add((XElement)el.FirstNode);
+        }
+
+        private void getCol(int i)
+        {
+            elements = from el in root.Descendants("column_" + i) select el;
+
+            foreach (XElement el in elements)
+            {
+                columns.Add(el.Value);
+            }
+        }
+
+        protected string getColValue(int i)
+        {
+            return columns[i];
+        }
+    }
+}
